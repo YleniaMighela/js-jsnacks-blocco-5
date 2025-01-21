62c46e45b26277cd7a40e59dd7bc3cc0767be109
Ylenia <yleniamighela@gmail.com>
2025-01-21 16:12:09 +0100

    added snack 01

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,3 +2,27 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+names.forEach((namesStudents) => {
+console.log(namesStudents);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// VERSIONE ESTESA PER ESERCITARMI
+
+// for(let i = 0; i < names.length; i++){
+
+//     namesStudents = names[i];
+//     console.log(namesStudents);
+// };
