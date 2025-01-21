e941ad16eadef933fc71a5d1329589d0021b0cbc
Ylenia <yleniamighela@gmail.com>
2025-01-21 17:06:17 +0100

    added snack 07

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,11 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const codiceStudente2 = students.find ((studente) => {
+
+  return studente.id === 2;
+
+});
+
+console.log(codiceStudente2);
