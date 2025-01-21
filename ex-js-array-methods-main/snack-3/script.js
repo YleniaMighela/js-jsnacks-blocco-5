e700a15281f178475d2749cdad7a0c72e6c7c14d
Ylenia <yleniamighela@gmail.com>
2025-01-21 16:31:56 +0100

    added snack 03

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,3 +2,28 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+
+const plusOne = numbers.map((numero) => {
+
+    return numero + 1;
+
+});
+
+console.log(plusOne);
+
+
+
+
+
+// ALTRA VERSIONE PER ESERCITARMI
+
+// const plusOne = [];
+
+// numbers.forEach((numero) => {
+
+//     plusOne.push(numero + 1);
+
+// });
+// console.log(plusOne);
+
